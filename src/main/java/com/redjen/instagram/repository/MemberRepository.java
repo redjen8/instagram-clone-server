76080fa55bc4dc6e5088230f52365c431ba03ae2
redjen8 <seokwoo2525@gmail.com>
2022-07-26 21:38:32 +0900

    20220726 jsw Feat : Member repository 최초 추가

---
+++ src/main/java/com/redjen/instagram/repository/MemberRepository.java
@@ -0,0 +1,26 @@
+package com.redjen.instagram.repository;
+
+import com.redjen.instagram.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void register(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOneById(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public Member findOneByMemberId(String id) {
+        return em.find(Member.class, id);
+    }
+}
