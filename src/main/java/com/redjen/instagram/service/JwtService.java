311f3013fdfabe3c122cd0d99724b87587557314
redjen8 <seokwoo2525@gmail.com>
2022-07-28 23:05:41 +0900

    20220728 jsw Feat : jwt 서비스 레이어 최초 추가

---
+++ src/main/java/com/redjen/instagram/service/JwtService.java
@@ -0,0 +1,41 @@
+package com.redjen.instagram.service;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Key;
+
+@Log4j2
+@Service
+public class JwtService {
+
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24; // 1일
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 7일
+    private final Key jwtKey;
+
+    public JwtService(@Value("${jwt.secret}") String jwtSecret) {
+        byte[] byteKey = Decoders.BASE64.decode(jwtSecret);
+        this.jwtKey = Keys.hmacShaKeyFor(byteKey);
+    }
+
+    public String getAccessTokenFromRequest() {
+        return "";
+    }
+
+    public String getRefreshTokenFromRequest() {
+        return "";
+    }
+
+    public String publishAccessToken() {
+        return "";
+    }
+
+    public String publishRefreshToken() {
+        return "";
+    }
+
+}
