a2e1c2e75ccdc19ef668978144ba6697f47ac9fe
redjen8 <seokwoo2525@gmail.com>
2022-07-28 23:05:49 +0900

    20220728 jsw Feat : jwt 서비스 테스트 클래스 생성

---
+++ src/test/java/com/redjen/instagram/jwt/JwtServiceTest.java
@@ -0,0 +1,10 @@
+package com.redjen.instagram.jwt;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("local")
+@SpringBootTest
+public class JwtServiceTest {
+
+}
