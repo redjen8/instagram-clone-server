a0eb072a8eb5aa327cc66c1f8afabae272b7bc62
redjen8 <seokwoo2525@gmail.com>
2022-07-29 22:54:30 +0900

    20220729 jsw Feat : 로그인 테스트 케이스 추가

---
+++ src/test/java/com/redjen/instagram/member/LoginTest.java
@@ -0,0 +1,36 @@
+package com.redjen.instagram.member;
+
+import com.redjen.instagram.service.JwtService;
+import com.redjen.instagram.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LoginTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    JwtService jwtService;
+
+    @DisplayName("사용자 첫 로그인")
+    @Test
+    public void memberFirstLoginTest() {
+
+    }
+
+    @DisplayName("access token 만료, refresh token 유효")
+    @Test
+    public void loginWithAccessExpiredAndRefreshValid() {
+
+    }
+
+    @DisplayName("access token 만료, refresh token 만료")
+    @Test
+    public void loginWithAccessExpiredAndRefreshExpired() {
+
+    }
+}
