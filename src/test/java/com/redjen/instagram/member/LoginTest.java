85cebd0f3b5a30ec255c8c80b2f7e69e29a4101e
redjen8 <seokwoo2525@gmail.com>
2022-07-31 20:47:16 +0900

    20220731 jsw Feat : 첫 로그인 테스트 케이스 작성

--- src/test/java/com/redjen/instagram/member/LoginTest.java
+++ src/test/java/com/redjen/instagram/member/LoginTest.java
@@ -1,11 +1,15 @@
 package com.redjen.instagram.member;
 
 import com.redjen.instagram.config.security.JwtProvider;
+import com.redjen.instagram.domain.member.MemberLoginReqParam;
+import com.redjen.instagram.domain.member.MemberLoginResultToken;
 import com.redjen.instagram.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class LoginTest {
@@ -19,7 +23,13 @@ public class LoginTest {
     @DisplayName("사용자 첫 로그인")
     @Test
     public void memberFirstLoginTest() {
-
+        MemberLoginReqParam loginParam = MemberLoginReqParam.builder()
+                .userId("test")
+                .password("1111")
+                .build();
+        MemberLoginResultToken loginResultToken = memberService.login(loginParam);
+        System.out.println(loginResultToken);
+        assertThat(loginResultToken).isNotNull();
     }
 
     @DisplayName("access token 만료, refresh token 유효")
