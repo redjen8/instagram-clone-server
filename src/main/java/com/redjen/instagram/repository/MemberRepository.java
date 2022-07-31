6cabd975addc5570837eb6890b0e1389188db491
redjen8 <seokwoo2525@gmail.com>
2022-07-31 23:21:27 +0900

    20220731 jsw Feat : querydsl 사용하여 user id 일치하는 사용자 찾는 메서드 생성

--- src/main/java/com/redjen/instagram/repository/MemberRepository.java
+++ src/main/java/com/redjen/instagram/repository/MemberRepository.java
@@ -1,52 +1,16 @@
 package com.redjen.instagram.repository;
 
-import com.redjen.instagram.domain.common.CustomException;
 import com.redjen.instagram.domain.common.CustomUserDetails;
-import com.redjen.instagram.domain.common.ErrorCode;
 import com.redjen.instagram.domain.entity.Member;
-import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.Optional;
 
-@Repository
-public class MemberRepository {
+public interface MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    void register(Member member);
+    Member findOneById(Long id);
+    Optional<Member> findOneByMemberId(String userId);
+    String updateRefreshToken(Long id, String newToken);
+    Optional<CustomUserDetails> convertMemberToCustomUserDetail(String userId);
 
-    public void register(Member member) {
-        em.persist(member);
-    }
-
-    public Member findOneById(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    public Optional<Member> findOneByMemberId(String userId) {
-        return Optional.of(em.find(Member.class, userId));
-    }
-
-    public String updateRefreshToken(Long id, String newToken) {
-        Member updateMember = em.find(Member.class, id);
-        updateMember.setRefreshToken(newToken);
-        return newToken;
-    }
-
-    public Optional<CustomUserDetails> convertMemberToCustomUserDetail(String userId) {
-        Member findMember;
-        Optional<Member> findMemberByMemberId = findOneByMemberId(userId);
-        if (findMemberByMemberId.isPresent()) {
-            findMember = findMemberByMemberId.get();
-        } else {
-            throw new CustomException(ErrorCode.LOGIN_ID_NOT_EXIST);
-        }
-        return Optional.ofNullable(CustomUserDetails.builder()
-                .id(userId)
-                .name(findMember.getName())
-                .password(findMember.getPassword())
-                .enabled(true)
-                .build());
-    }
 }
---
+++ src/main/java/com/redjen/instagram/repository/MemberRepositoryImpl.java
@@ -0,0 +1,63 @@
+package com.redjen.instagram.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.redjen.instagram.domain.common.CustomException;
+import com.redjen.instagram.domain.common.CustomUserDetails;
+import com.redjen.instagram.domain.common.ErrorCode;
+import com.redjen.instagram.domain.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Optional;
+
+import static com.redjen.instagram.domain.entity.QMember.member;
+
+@Repository
+public class MemberRepositoryImpl implements MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public MemberRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
+        this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    public void register(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOneById(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public Optional<Member> findOneByMemberId(String userId) {
+        return Optional.ofNullable(jpaQueryFactory.selectFrom(member)
+                .where(member.id.eq(userId))
+                .fetchOne());
+    }
+
+    public String updateRefreshToken(Long id, String newToken) {
+        Member updateMember = em.find(Member.class, id);
+        updateMember.setRefreshToken(newToken);
+        return newToken;
+    }
+
+    public Optional<CustomUserDetails> convertMemberToCustomUserDetail(String userId) {
+        Member findMember;
+        Optional<Member> findMemberByMemberId = findOneByMemberId(userId);
+        if (findMemberByMemberId.isPresent()) {
+            findMember = findMemberByMemberId.get();
+        } else {
+            throw new CustomException(ErrorCode.LOGIN_ID_NOT_EXIST);
+        }
+        return Optional.ofNullable(CustomUserDetails.builder()
+                .id(userId)
+                .name(findMember.getName())
+                .password(findMember.getPassword())
+                .enabled(true)
+                .build());
+    }
+}
