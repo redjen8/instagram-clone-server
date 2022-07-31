00438ace8918108ff8f9c4bbca2edaf4aee992ed
redjen8 <seokwoo2525@gmail.com>
2022-07-31 20:43:28 +0900

    20220731 jsw Feat : Member user id로 검색 시 Optional 객체를 반환하도록 변경

--- src/main/java/com/redjen/instagram/repository/MemberRepository.java
+++ src/main/java/com/redjen/instagram/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.redjen.instagram.repository;
 
+import com.redjen.instagram.domain.common.CustomException;
 import com.redjen.instagram.domain.common.CustomUserDetails;
+import com.redjen.instagram.domain.common.ErrorCode;
 import com.redjen.instagram.domain.entity.Member;
 import org.springframework.stereotype.Repository;
 
@@ -22,8 +24,8 @@ public class MemberRepository {
         return em.find(Member.class, id);
     }
 
-    public Member findOneByMemberId(String userId) {
-        return em.find(Member.class, userId);
+    public Optional<Member> findOneByMemberId(String userId) {
+        return Optional.of(em.find(Member.class, userId));
     }
 
     public String updateRefreshToken(Long id, String newToken) {
@@ -33,7 +35,13 @@ public class MemberRepository {
     }
 
     public Optional<CustomUserDetails> convertMemberToCustomUserDetail(String userId) {
-        Member findMember = findOneByMemberId(userId);
+        Member findMember;
+        Optional<Member> findMemberByMemberId = findOneByMemberId(userId);
+        if (findMemberByMemberId.isPresent()) {
+            findMember = findMemberByMemberId.get();
+        } else {
+            throw new CustomException(ErrorCode.LOGIN_ID_NOT_EXIST);
+        }
         return Optional.ofNullable(CustomUserDetails.builder()
                 .id(userId)
                 .name(findMember.getName())
