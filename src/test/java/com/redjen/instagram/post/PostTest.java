9de63014e219d5a8fe3ac237635b7e21921f3f98
redjen8 <seokwoo2525@gmail.com>
2022-08-01 21:03:03 +0900

    20220801 jsw Feat : 게시글 테스트 케이스 추가

---
+++ src/test/java/com/redjen/instagram/post/PostTest.java
@@ -0,0 +1,31 @@
+package com.redjen.instagram.post;
+
+import com.redjen.instagram.service.PostService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PostTest {
+
+    private PostService postService;
+
+    @Test
+    public void writeNewPostTest() {
+
+    }
+
+    @Test
+    public void deleteNewPostTest() {
+
+    }
+
+    @Test
+    public void modifyPostTest() {
+
+    }
+
+    @Test
+    public void getPostListTest() {
+
+    }
+}
