49fea8eae1172088c77204b58e3dfd79178e1f3a
redjen8 <seokwoo2525@gmail.com>
2022-08-01 23:46:24 +0900

    20220801 jsw Feat : 게시글 테스트 시 사용할 공통 멤버 객체 초기화

--- src/test/java/com/redjen/instagram/post/PostTest.java
+++ src/test/java/com/redjen/instagram/post/PostTest.java
@@ -1,13 +1,35 @@
 package com.redjen.instagram.post;
 
+import com.redjen.instagram.domain.entity.Member;
 import com.redjen.instagram.service.PostService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
 
 @SpringBootTest
 public class PostTest {
 
     private PostService postService;
+    private PasswordEncoder passwordEncoder;
+
+    private Member testMember;
+
+    @PostConstruct
+    public void init() {
+        testMember = Member.builder()
+                .userId("test")
+                .name("test")
+                .password(passwordEncoder.encode("1234"))
+                .isSSO(false)
+                .isPublic(true)
+                .isActive(true)
+                .birthDate(new Date())
+                .phoneNumber("010-1111-1111")
+                .build();
+    }
 
     @Test
     public void writeNewPostTest() {
