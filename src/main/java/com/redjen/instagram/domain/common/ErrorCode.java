298aa31d4df1fa1b9d24564e3c7ef6ad22414185
redjen8 <seokwoo2525@gmail.com>
2022-08-02 23:36:01 +0900

    20220802 jsw Feat : 게시글 작성 및 수정 시 사용자 ID 검증 에러 코드 추가

--- src/main/java/com/redjen/instagram/domain/common/ErrorCode.java
+++ src/main/java/com/redjen/instagram/domain/common/ErrorCode.java
@@ -12,7 +12,9 @@ public enum ErrorCode {
     LOGIN_ID_NOT_EXIST (HttpStatus.BAD_REQUEST, "해당 아이디가 존재하지 않습니다."),
     LOGIN_PASSWORD_NOT_EQUAL (HttpStatus.BAD_REQUEST, "아이디와 비밀번호가 일치하지 않습니다."),
     INVALID_FORMAT_TOKEN (HttpStatus.BAD_REQUEST, "jwt 토큰의 형식이 유효하지 않습니다."),
-    EXPIRED_ACCESS_TOKEN (HttpStatus.UNAUTHORIZED, "만료된 jwt 토큰입니다.");
+    EXPIRED_ACCESS_TOKEN (HttpStatus.UNAUTHORIZED, "만료된 jwt 토큰입니다."),
+    POST_MEMBER_ID_NOT_EXIST (HttpStatus.BAD_REQUEST, "해당 아이디가 존재하지 않습니다."),
+    POST_MODIFY_MEMBER_ID_UNAUTHORIZED (HttpStatus.UNAUTHORIZED, "본인이 작성한 게시글만 수정할 수 있습니다.");
 
     private final HttpStatus status;
     private final String message;
